a3413cbb4189a9beec9f213de6ea3435cdd9305a
dalalEg <49488556+dalalEg@users.noreply.github.com>
2019-06-14 00:25:06 +0300

    Update eurovision.h

--- eurovision.h
+++ eurovision.h
@@ -110,7 +110,13 @@ public :
     MainControl(int max_time = 180, int max_participants = 26, int max_votes = 5);
     ~MainControl();
     MainControl& operator+=(Participant&);
+    /////////////////////////////////////
+    MainControl& operator-=(Participant&);
+    void setPhase (int new_phase);
+    bool legalParticipant(Participant& participant)const ;
+    bool participate (const  string) ;
     friend std::ostream &operator<<(std::ostream&, const MainControl&);
+
 };
 
 
