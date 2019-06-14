408187f6f544a54cb8ab3fa9d4adba7b5bf0be6b
yasmeen16-meet <yasmeensaadi99@gmail.com>
2019-06-14 15:55:56 +0300

    Update eurovision.cpp

--- eurovision.cpp
+++ eurovision.cpp
@@ -312,3 +312,30 @@ Vote::Vote(Voter voter, string state) {
 Vote::~Vote() {
     this->voter = NULL;
 }
+
+
+///rankToScore function
+int rankToScore(int rank){
+
+    switch (rank){
+        case 0:
+            return 12;
+        case 1:
+            return 10;
+        case 2:
+            return 8;
+        case 3:
+            return 7;
+        case 4:
+            return 6;
+        case 5:
+            return 5;
+        case 6:
+            return 4;
+        case 7:
+            return 3;
+        case 8:
+            return 2;
+    }
+    return 1;
+}
