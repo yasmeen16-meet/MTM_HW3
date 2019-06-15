58f004fd47166f793a798b852b75766e936c0b1a
dalalEg <49488556+dalalEg@users.noreply.github.com>
2019-06-15 12:03:21 +0300

    Update eurovision.h

--- eurovision.h
+++ eurovision.h
@@ -3,8 +3,7 @@
 
 #include <iostream>
 #include <string.h>
-// it's allowed to define here any using statements, according to needs.
-// do NOT define here : using namespace std;
+
 //---------------------------------------------------
 enum VoterType { All, Regular, Judge };
 enum Phase { Registration, Contest, Voting };
@@ -13,7 +12,6 @@ using namespace std ;
 //---------------------------------------------------
 
 class Participant {
-// relevant private members can be defined here, if necessary.
 private:
     string state_name;
     string song_name;
@@ -45,70 +43,42 @@ public :
     void update(string new_song, int new_time, string new_singer);
 
     void updateRegistered(bool);
-// need to define here possibly c'tr and d'tr and ONLY methods that
-
-
-// NO friend is allowed here.
-
-
-// NO OTHER METHODS SHOULD APPEAR HERE.
-// are mentioned and demonstrated in the test example that has been published.
-
 };
 std::ostream& operator<<(std::ostream& , const Participant&);
 
 //---------------------------------------------------
 
-
 class Voter
 {
-// relevant private members can be defined here, if necessary.
     int type , counter;
     string state_name;
-
-
 public :
-// need to define here possibly c'tr and d'tr and ONLY methods that
-// are mentioned and demonstrated in the test example that has been published.
-// NO friend is allowed here.
     Voter (string, int type =Regular);
     string state()const ;
     int voterType()const ;
     int timesOfVotes()const ;
     Voter& operator++( );
-
 };
 std::ostream& operator<<(std::ostream& , const Voter&);
-
 
 // -----------------------------------------------------------
 
 struct Vote
 {
-
     Voter* voter;
-    string state;
-
-    Vote(Voter, string);
+    string* state;
+    Vote(Voter&, string="", string="",string ="",string="",string ="",string="",string="", string =""
+            ,string="" , string= "");
     ~Vote();
-
-// ALL is public here.
-// need to define ONLY data members and c'tr and d'tr.
-// NO NEED to define anything else.
-
 };
 
 // -----------------------------------------------------------
 
-
 class MainControl {
     int max_time, max_participants, max_votes, phase;
     Participant **control_participants;
-
     int* regular_votes, *judge_votes;
 
-    //votes
-// relevant private members can be defined here, if necessary.
 
 private:
     int getSize()const;
@@ -116,16 +86,9 @@ private:
     ///
     Participant** sortParticipants();
     void sortHelp(Participant**, int);
-
+    int getIndex(string);
 
 public :
-
-// need to define here possibly c'tr and d'tr and ONLY methods that
-// are mentioned and demonstrated in the test example that has been published.
-// NO OTHER METHODS SHOULD APPEAR HERE.
-
-// Also it's allowed here to define friend.
-
     MainControl(int max_time = 180, int max_participants = 26, int max_votes = 5);
     ~MainControl();
     MainControl& operator+=(Participant&);
@@ -135,6 +98,7 @@ public :
     bool legalParticipant(Participant& participant)const ;
     bool participate (const  string) ;
     friend std::ostream &operator<<(std::ostream&, const MainControl&);
+    MainControl& operator+=(Vote );
 };
 
 
