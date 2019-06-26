eeda3d7efa9bfd2a38b31d9f61943ba5ff2c303e
dalalEg <49488556+dalalEg@users.noreply.github.com>
2019-06-26 15:41:32 +0300

    Update seat.cpp

--- seat.cpp
+++ seat.cpp
@@ -1,11 +1,31 @@
-//
-// Created by HD on 25/06/2019.
-//
 
 #include "seat.h"
 using std::to_string;
 
-/////////FrontRegularSeat/////////
+///GoldenCircleSeat
+string GoldenCircleSeat::location() const {
+    string line2 =to_string(this->line);
+    string chair2 =to_string(this->chair);
+    string str = "Golden Circle-> line: "+ line2+ ", chair: "+ chair2;
+    return str;
+}
+
+int GoldenCircleSeat::price() const {
+    return this->base_price;
+}
+
+///DisablePodiumSeat
+string DisablePodiumSeat::location() const {
+    string line2 =std::to_string(this->line);
+    string chair2 =std::to_string(this->chair);
+    string str ="Disable Podium-> line: "+line2+", chair: "+chair2;
+    return str;
+}
+int DisablePodiumSeat::price() const {
+    return this->base_price;
+}
+
+////////FrontRegularSeat/////////
 int FrontRegularSeat::price() const {
     return this->base_price;
 }
@@ -13,7 +33,7 @@ int FrontRegularSeat::price() const {
 string FrontRegularSeat::location() const {
     string area_str(1,this->area);
     string result = "Front-> area: " + area_str + ", line: " + to_string(this->line)
-            + ", chair: " + to_string(this->chair);
+                    + ", chair: " + to_string(this->chair);
     return result;
 }
 ///////////////
@@ -39,4 +59,23 @@ string RearRegularSeat::location() const {
     string result = "Rear-> area: " + area_str + ", line: " + to_string(this->line)
                     + ", chair: " + to_string(this->chair);
     return result;
+}
+
+////GreenRoomSeat
+string GreenRoomSeat::location() const {
+    string line2 =to_string(this->line);
+    string chair2 =to_string(this->chair);
+    string str = "Green Room-> line: "+ line2+ ", chair: "+ chair2;
+    return str;
+}
+
+int GreenRoomSeat::price() const {
+    throw NoPrice();
+}
+
+///////NoPrice//////
+
+const char* NoPrice::what() const noexcept{
+    return "Not For Sale !";
+
 }
