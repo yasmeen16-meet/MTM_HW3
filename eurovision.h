9325bf9f37d7eb07cd02acf15bbcf9c883b303f9
yasmeen16-meet <yasmeensaadi99@gmail.com>
2019-06-30 19:10:20 +0300

    Update eurovision.h

--- eurovision.h
+++ eurovision.h
@@ -18,7 +18,7 @@ private:
     int song_length;
     string singer_name;
     int is_registerd;
-    
+
 
 public :
     Participant(string state, string song, int length, string singer);
@@ -49,27 +49,31 @@ std::ostream& operator<<(std::ostream& , const Participant&);
 
 //---------------------------------------------------
 
-class Voter
-{
-    int type , counter;
+class Voter {
+    int type, counter;
     string state_name;
 public :
-    Voter (string, int type =Regular);
-    string state()const ;
-    int voterType()const ;
-    int timesOfVotes()const ;
-    Voter& operator++( );
+    Voter(string, int type = Regular);
+
+    string state() const;
+
+    int voterType() const;
+
+    int timesOfVotes() const;
+
+    Voter &operator++();
 };
 std::ostream& operator<<(std::ostream& , const Voter&);
 
 // -----------------------------------------------------------
 
-struct Vote
-{
-    Voter* voter;
-    string* state;
-    Vote(Voter&, string="", string="",string ="",string="",string ="",string="",string="", string =""
-            ,string="" , string= "");
+struct Vote {
+    Voter *voter;
+    string *state;
+
+    Vote(Voter &, string= "", string= "", string = "", string= "", string = "", string= "", string= "", string = "",
+         string= "", string= "");
+
     ~Vote();
 };
 
@@ -78,54 +82,82 @@ struct Vote
 class MainControl {
     int max_time, max_participants, max_votes, phase;
     Participant **control_participants;
-    int* regular_votes, *judge_votes;
+    int *regular_votes, *judge_votes;
 
 
 private:
-    int getSize()const;
-    bool stateExists(const MainControl& , string state);
+    int getSize() const;
+
+    bool stateExists(const MainControl &, string state);
+
     ///
-    Participant** sortParticipants();
-    void sortHelp(Participant**, int);
+    Participant **sortParticipants();
+
+    void sortHelp(Participant **, int);
+
     int getIndex(string);
-    MainControl& operator =(const MainControl&)= default;
+
+    MainControl &operator=(const MainControl &) = default;
 
 public :
     MainControl(int max_time = 180, int max_participants = 26, int max_votes = 5);
+
     ~MainControl();
-    MainControl& operator+=(Participant&);
-    friend std::ostream &operator<<(std::ostream&, MainControl&);
-    MainControl& operator-=(Participant&);
-    void setPhase (int new_phase);
-    bool legalParticipant(Participant& participant)const ;
-    bool participate (const  string) ;
-    friend std::ostream &operator<<(std::ostream&, const MainControl&);
-    MainControl& operator+=(Vote );
+
+    MainControl &operator+=(Participant &);
+
+    friend std::ostream &operator<<(std::ostream &, MainControl &);
+
+    MainControl &operator-=(Participant &);
+
+    void setPhase(int new_phase);
+
+    bool legalParticipant(Participant &participant) const;
+
+    bool participate(const string);
+
+    friend std::ostream &operator<<(std::ostream &, const MainControl &);
+
+    MainControl &operator+=(Vote);
 
     ///////part2/////
-    MainControl(const MainControl& main_control);
+    MainControl(const MainControl &main_control);
+
     class Iterator;
+
     Iterator begin() const;
+
     Iterator end() const;
+
     string operator()(int i, int voter_type) const;
 
 
-    };
+};
 
 class MainControl::Iterator {
-    const MainControl* main_control;
+    const MainControl *main_control;
     int index;
-    Iterator (const MainControl* main_control, int index);
+
+    Iterator(const MainControl *main_control, int index);
+
     friend class MainControl;
+
 public:
     Iterator();
-    const Participant& operator*()const;
-    Iterator& operator++();
-    bool operator<(const Iterator& it) const;
-    bool operator==(const Iterator& it) const;
-    Iterator(const Iterator&) = default;
-    Iterator& operator=(const Iterator&)= default ;
-    Iterator& operator--() = delete;
+
+    const Participant &operator*() const;
+
+    Iterator &operator++();
+
+    bool operator<(const Iterator &it) const;
+
+    bool operator==(const Iterator &it) const;
+
+    Iterator(const Iterator &) = default;
+
+    Iterator &operator=(const Iterator &) = default;
+
+    Iterator &operator--() = delete;
 };
 
 
